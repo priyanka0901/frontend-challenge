b63ebfb15a7d8d5b93c228e2822fbf4f90b58e06
priyanka0901 <gupta.priyanka0901@gmail.com>
2017-03-16 13:39:59 +0530

    frontend assignment complete

--- public/client.js
+++ public/client.js
@@ -1,16 +1,16 @@
 /*-----country request-----*/
 var Orequest = new XMLHttpRequest();
-Orequest.onload =function(e){
+Orequest.onload = function(e) {
     var ajaxdata = Orequest.response;
-    for(var i = 0; i < 9; i++){
+    for (var i = 0; i < 9; i++) {
         var countries = ajaxdata.countries[i];
-          // console.log(countries);
-          $('#list1').append("<option class='country-options'>"+ countries +"</option>");
-          $('#list2').append("<option class='country-options'>"+ countries +"</option>");
+        // console.log(countries);
+        $('#list1').append("<option class='country-options'>" + countries + "</option>");
+        $('#list2').append("<option class='country-options'>" + countries + "</option>");
     }
 };
-Orequest.open('GET','/countries/');
-Orequest.responseType="json";
+Orequest.open('GET', '/countries/');
+Orequest.responseType = "json";
 Orequest.send();
 
 /*------country trends request-------*/
@@ -22,11 +22,11 @@ $('#list1').change(function() {
     var ajaxdata1 = $(this).find(':selected')[0].innerHTML;
     // console.log(ajaxdata);
     $.ajax({
-        type:'GET',
-        url:'/countries/'+ ajaxdata1 + '/trends/',
-        success:function(data){
-            for(var i = 0; i< data.trends.length; i++){
-                namedata1.push(data.trends[i].name); 
+        type: 'GET',
+        url: '/countries/' + ajaxdata1 + '/trends/',
+        success: function(data) {
+            for (var i = 0; i < data.trends.length; i++) {
+                namedata1.push(data.trends[i].name);
             }
             compareArray(namedata1, namedata2);
             // console.log(namedata1);
@@ -40,32 +40,32 @@ $('#list2').change(function() {
     var ajaxdata2 = $(this).find(':selected')[0].innerHTML;
     // console.log(ajaxdata);
     $.ajax({
-        type:'GET',
-        url:'/countries/'+ ajaxdata2 + '/trends/',
-        success:function(data){
-            for(var i = 0; i< data.trends.length; i++){
+        type: 'GET',
+        url: '/countries/' + ajaxdata2 + '/trends/',
+        success: function(data) {
+            for (var i = 0; i < data.trends.length; i++) {
                 namedata2.push(data.trends[i].name);
             }
             compareArray(namedata1, namedata2);
-             // console.log(namedata2); 
+            // console.log(namedata2); 
         }
     })
 });
 
-function compareArray(arr1, arr2) { 
+function compareArray(arr1, arr2) {
     namedata3 = [];
-    if(arr1.length === 0){
-        for(var i = 0; i < arr2.length; i++){
+    if (arr1.length === 0) {
+        for (var i = 0; i < arr2.length; i++) {
             namedata3.push(arr2[i]);
         }
-    }else if(arr2.length === 0){
-        for(var i = 0; i < arr1.length; i++){
+    } else if (arr2.length === 0) {
+        for (var i = 0; i < arr1.length; i++) {
             namedata3.push(arr1[i]);
         }
-    }else {
-        for(var i = 0; i < arr1.length; i++){
-            for(var j = 0; j < arr2.length; j++){
-                if(arr1[i] === arr2[j]){
+    } else {
+        for (var i = 0; i < arr1.length; i++) {
+            for (var j = 0; j < arr2.length; j++) {
+                if (arr1[i] === arr2[j]) {
                     namedata3.push(arr1[i]);
                 }
             }
@@ -76,57 +76,63 @@ function compareArray(arr1, arr2) {
 
 function appendTrends() {
     var trends = '';
-    for(var i = 0; i < namedata3.length; i++){
+    for (var i = 0; i < namedata3.length; i++) {
         trends += "<p class='trends-menu'>" + namedata3[i] + "</p>";
     }
     $('.trends').html(trends);
     arrayLength();
-    stringLength();
+    percentCal();
     graphmaker();
 }
 
 /*------array length---*/
 var totallength;
+
 function arrayLength() {
     totallength = 0;
-    for(var i = 0; i< namedata3.length; i++){
+    for (var i = 0; i < namedata3.length; i++) {
         totallength += namedata3[i].length;
     }
     // console.log(totallength);
 }
 
-/*---single string length--*/
-var stringlength;
-function stringLength() {
-    stringlength = 0;
-    for(var i = 0; i< namedata3.length; i++) {
-        stringlength = namedata3[i].length;
-        // console.log(stringlength);
+/*-----percent calculation------*/
+var trendweight;
+
+function percentCal() {
+    trendweight = [];
+    for (var i = 0; i < namedata3.length; i++) {
+        trendweight.push(((namedata3[i].length / totallength) * 100).toFixed(2));
     }
+    console.log(trendweight);
 }
 
 /*------chart---------*/
 
 function graphmaker(strlength, arrlength) {
-        var ctx = document.getElementById("myChart").getContext('2d');
-        var myChart = new Chart(ctx, {
-          type: 'pie',
-          data: {
+    $('.result-graph').html('');
+    var canvasElem = '<canvas id="myChart"></canvas>';
+    $('.result-graph').html(canvasElem);
+    var ctx = document.getElementById("myChart").getContext('2d');
+    var myChart = new Chart(ctx, {
+        type: 'pie',
+        data: {
             labels: namedata3,
             datasets: [{
-              backgroundColor: [
-                "#2ecc71",
-                "#ff3b3b"
-              ],
-              data: [10,12]
+                backgroundColor: [
+                    "#5b066b",
+                    "#e1067c",
+                    "#f46f00",
+                    "#116063",
+                    "#40f906",
+                    "#fb6c6c",
+                    "#1c1d78",
+                    "#f68b8b",
+                    "#cfa63d",
+                    "#305434"
+                ],
+                data: trendweight
             }]
-          }
-        });
-    };
-
-
-
-
-
-
-
+        }
+    });
+};
